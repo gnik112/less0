78973203f5dde2bf9a75187e5fd22d4ae22df0a5
gnik112 <gnik112@yandex.ru>
2024-07-02 20:00:44 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_homework_assignments
+print('Курс:', course_name + ', всего задач:', str(number_of_completed_homework_assignments) + ', затрачено часов:', str(number_of_hours_spent) + ', среднее время выполнения:', str(time_for_one_task), 'часа.')
