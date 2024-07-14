25f216b610a01709c2bb3d995add394d84cc24ff
gnik112 <gnik112@yandex.ru>
2024-07-14 10:33:08 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,22 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for list_i in list_to_search:
+        if string.lower() == str(list_i).lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
