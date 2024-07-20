2f685c51808d3ed7d5c26af70f4398736cde5aa9
gnik112 <gnik112@yandex.ru>
2024-07-20 17:41:25 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    # Также будет логичным пропустить нули в конце числа, например - 4020300
+    if len(str_number) > 1 and int(str_number[1:]) > 0:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    return first
+
+result = get_multiplied_digits(40203)
+print(result)
+
+result = get_multiplied_digits(40203000)
+print(result)
