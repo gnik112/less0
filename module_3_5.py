1276d5da07865bbd9cc917ed5b2505ae607bd4c7
gnik112 <gnik112@yandex.ru>
2024-07-21 09:23:37 +0300

    Дописал комментарий

--- module_3_5.py
+++ module_3_5.py
@@ -3,6 +3,7 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     # Также будет логичным пропустить нули в конце числа, например - 4020300
+    # Из-за "отложенной оценки" в проверке and - ошибки не будет
     if len(str_number) > 1 and int(str_number[1:]) > 0:
         return first * get_multiplied_digits(int(str_number[1:]))
     return first
