5648efca57c56af1089f210ef288147b58ec6614
gnik112 <gnik112@yandex.ru>
2024-11-24 14:50:39 +0300

    Add files via upload

---
+++ crud_functions_5.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+connection = sqlite3.connect("telegram_bot_5.db")
+cursor = connection.cursor()
+
+
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+
+    connection.commit()
+
+
+def get_all_products():
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+    return products
+
+
+def add_user(username, email, age):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (username, email, age, 1000))
+    connection.commit()
+
+
+def is_included(username):
+    users = cursor.execute("SELECT COUNT(*) FROM Users WHERE username = ?", (username,)).fetchone()
+    connection.commit()
+    return (True if users[0] != 0 else False)
