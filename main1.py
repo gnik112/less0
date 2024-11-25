58949d40c8d491c4358dcb8fde306647f20ed95e
gnik112 <gnik112@yandex.ru>
2024-11-25 21:31:04 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,19 @@
+# 1st program
+print('Задача 1:')
+print(9 ** .5 * 5)
+
+# 2nd program
+print('\r\nЗадача 2:')
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+# 3rd program
+print('\r\nЗадача 3:')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4rd program
+print('\r\nЗадача 4:')
+str1 = '123.456'
+dt4 = int(float(str1) * 10 // 1 % 10)
+print(dt4)
